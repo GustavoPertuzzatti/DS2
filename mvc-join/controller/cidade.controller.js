b4341fe9e2193c78620ff2b71c12dc37178a8093
Gustavopertuzzatti <gustavoleonardi@outlook.com>
2019-08-27 21:30:55 -0300

    27-08-pt2

--- mvc-join/controller/cidade.controller.js
+++ mvc-join/controller/cidade.controller.js
@@ -3,7 +3,7 @@ const cidade = [];
 
 module.exports = {
 
-    find: (req,res) => {
+    find: (req, res) => {
 
         repository.find((error, result) => {
             if (error) {
@@ -28,11 +28,11 @@ module.exports = {
 
             res.send(cidades);
         });
-        
+
     },
 
-    findByID: (req,res) => {
-       
+    findByID: (req, res) => {
+
         repository.findById(req.params, (error, result) => {
             if (error) {
                 res.status(500).send(error);
@@ -56,66 +56,57 @@ module.exports = {
 
             res.send(cidades);
 
-             
-                            
         });
 
     },
 
-    create: (req,res) => {
+    create: (req, res) => {
 
-            // Converte de objeto para relacional
-            const cidade = {
-                nome: req.body.nome,
-                estado_id: req.body.estado.id
+        // Converte de objeto para relacional
+        const cidade = {
+            nome: req.body.nome,
+            estado_id: req.body.estado.id
+        }
+
+        repository.create(cidade, (error, result) => {
+            if (error) {
+                res.status(500).send(error);
             }
-
-            repository.create(cidade, (error, result) => {
-                if (error) {
-                    res.status(500).send(error);
-                }
 
             res.send(result);
         });
 
     },
 
-    update: (req,res) => {
-        //Atualiza o id do objeto do req.body
-        req.body.id = req.params.id;
-
+    update: (req, res) => {
         // Converte de objeto para relacional
         const cidade = {
-        id: req.params.id,  
-        nome: req.body.nome,
-        estado_id: req.body.estado.id
-    }
+            id: req.params.id,
+            nome: req.body.nome,
+            estado_id: req.body.estado.id
+        }
 
         repository.update(cidade, (error, result) => {
             if (error) {
                 res.status(500).send(error);
             }
 
-            if (result.affectedRows == 0 ) {
-                res.status(404).send();                
+            if (result.affectedRows == 0) {
+                res.status(404).send();
             }
-
- 
-
 
             res.send(result);
         });
     },
 
-    delete: (req,res) => {
+    delete: (req, res) => {
         repository.delete(req.params, (error, result) => {
             if (error) {
                 res.status(500).send(error);
             }
-            
 
             res.status(204).send();
-        });  
+        });
     }
 
 }
