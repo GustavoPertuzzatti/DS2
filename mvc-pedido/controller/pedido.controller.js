92081ba102e8df913a9db3ab13ddaf471d2a0a9b
Gustavopertuzzatti <gustavoleonardi@outlook.com>
2019-09-13 22:05:00 -0300

    13-09

--- mvc-pedido/controller/pedido.controller.js
+++ mvc-pedido/controller/pedido.controller.js
@@ -1,11 +1,8 @@
 const repository = require('../repository/pedido.repository');
-const itemRepository = require('../repository/itemPedido.repository');
 const moment = require('moment');
 const pedido = [];
 
-//////////////////////////////////////////////////////////////////////////
-//               AINDA NÃO AJUSTEI O RESTANTE DO ARQUIVO               ///
-//////////////////////////////////////////////////////////////////////////
+
 module.exports = {
 
     find: (req, res) => {
@@ -35,8 +32,8 @@ module.exports = {
                         codigo: item.VENDEDOR_CODIGO,
                         nome: item.VENDEDOR_NOME,
                         email: item.VENDEDOR_EMAIL
-                    }
-            /// Aqui virá a chado do itemPedido, retornando os itens do pedido (PRetendo implementar isso indo para ctba)              
+                    },
+                    itens: item.itens               
                 }
                 pedidoes.push(pedido);
             };
@@ -50,24 +47,47 @@ module.exports = {
         repository.findById(req.params, (error, result) => {
             if (error) {
                 res.status(500).send(error);
-            };
+            }       
 
-            const pedidos = [];
+            const pedidoes = [];
 
             // Converte de relacional para objeto
             for (item of result) {
                 let pedido = {
+                    id: item.ID,
                     codigo: item.CODIGO,
-                    dtpedido: item.dtpedido,
-                    observacao: item.observacao,
-                    cliente_id: item.cliente_id
+                    dtpedido: moment(item.DTPEDIDO).format('YYYY-MM-DD'),
+                    observacao: item.OBSERVACAO,
+                    cliente: {
+                        id: item.CLIENTE_ID,
+                        codigo: item.CLIENTE_CODIGO,
+                        nome: item.CLIENTE_NOME,
+                        email: item.CLIENTE_EMAIL
+                    },
+                    vendedor: {
+                        id: item.VENDEDOR_ID,
+                        codigo: item.VENDEDOR_CODIGO,
+                        nome: item.VENDEDOR_NOME,
+                        email: item.VENDEDOR_EMAIL
+                    },
+                    itens: item.itens               
                 }
-                pedidos.push(pedido);
-            }
-            res.send(pedidos);
-        })
+                pedidoes.push(pedido);
+            };
+
+            res.send(pedidoes);
+        });
+
     },
     create: (req, res) => {
+        
+
+        const pedido = {
+            nome: req.body.nome,
+            estado_id: req.body.estado.id
+        }
+
+
 
     },
 
